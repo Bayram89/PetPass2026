9966801dd35133acebc9154d2e31a94a1c0a563e
Bayram Erdem <bayram9erdem@gmail.com>
2026-05-24 13:52:02 +0200

    Do not lowercase Next static assets

--- app-next/middleware.js
+++ app-next/middleware.js
@@ -2,9 +2,20 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   const url = request.nextUrl.clone();
+  const path = url.pathname;
+
+  if (
+    path.startsWith("/_next") ||
+    path.startsWith("/api") ||
+    path.startsWith("/auth") ||
+    path.includes(".")
+  ) {
+    return NextResponse.next();
+  }
+
   const lowerPath = url.pathname.toLowerCase();
 
-  if (url.pathname !== lowerPath) {
+  if (path !== lowerPath) {
     url.pathname = lowerPath;
     return NextResponse.redirect(url);
   }
